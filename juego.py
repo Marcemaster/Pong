0110f4860eb33d0f71fb64e5f10b521e821158a9
Marcelo Hornillos <marcehornillos@gmail.com>
2021-08-31 23:09:42 +0200

    ultimo commit faltan cosas por corregir

--- juego.py
+++ juego.py
@@ -41,7 +41,7 @@ class Movil():
     @property
     def abajo(self):
         return self.y + self.h
-    
+
     @abajo.setter
     def abajo(self, valor):
         self.y = valor - self.h
@@ -53,7 +53,7 @@ class Movil():
         pg.draw.rect(lienzo, self.color,
                      pg.Rect(self.x, self.y, self.w, self.h))
 
-    def procesa_eventos(self, lista_eventos):
+    def procesa_eventos(self, lista_eventos=[]):
         pass
 
 
@@ -76,6 +76,7 @@ class Raqueta(Movil):
         if self.abajo >= TAMANNO[1]:
             self.abajo = TAMANNO[1]
 
+
 class Bola(Movil):
     def __init__(self, x, y, color=(255, 255, 255)):
         Movil.__init__(self, x, y, 20, 20, color)
@@ -86,11 +87,25 @@ class Bola(Movil):
 
     def actualizate(self):
 
-        if self.derecha > TAMANNO[0] or self.x < 0:
-            self.incremento_x *= -1
+        if self.swDerecha:
+            self.x += 5
+        else:
+            self.x -= 5
 
-        if self.y + self.h > TAMANNO[1] or self.y < 0:
-            self.incremento_y *= -1
+        if self.swArriba:
+            self.y -= 5
+        else:
+            self.y += 5
+
+        if self.abajo >= TAMANNO[1]:
+            self.swArriba = True
+
+        if self.arriba <= 0:
+            self.swArriba = False
+
+    def comprobar_choque(self, algo):
+        return self.derecha >= algo.izquierda and self.izquierda <= algo.derecha and self.izquierda \
+            >= algo.arriba and self.arriba <= algo.abajo
 
 
 class Game():
@@ -100,16 +115,18 @@ class Game():
         self.todos = []
 
         self.player1 = Raqueta(10, (TAMANNO[1] - 120)//2)
+        self.player1.tecla_arriba = pg.K_w
+        self.player1.tecla_abajo = pg.K_s
         self.player2 = Raqueta(TAMANNO[0]-30, (TAMANNO[1] - 120) // 2)
 
         self.todos.append(self.player1)
         self.todos.append(self.player2)
 
-        bola = Bola(TAMANNO[0] // 2 - 10,
+        self.bola = Bola(TAMANNO[0] // 2 - 10,
                     TAMANNO[1] // 2 - 10,
                     (255, 255, 0))
-        self.todos.append(bola)
 
+        self.todos.append(self.bola)
 
     def bucle_principal(self):
         game_over = False
@@ -122,18 +139,15 @@ class Game():
             for evento in eventos:
                 if evento.type == pg.QUIT:
                     game_over = True
-                
-                if pg.key.get_pressed()[pg.K_UP]:
-                    self.player1.y -= 5
-                if pg.key.get_pressed()[pg.K_DOWN]:
-                    self.player1.y += 5
-                if pg.key.get_pressed()[pg.K_w]:
-                    self.player2.y -= 5
-                if pg.key.get_pressed()[pg.K_s]:
-                    self.player2.y += 5
 
             for movil in self.todos:
                 movil.procesa_eventos()
+
+                if self.bola.comprobar_choque(self.player1) or \
+                        self.bola.comprobar_choque(self.player2):
+                    self.bola.swDerecha = not self.bola.swDerecha
+
+            for movil in self.todos:
                 movil.actualizate()
 
             self.pantalla.fill((0, 0, 0))
@@ -142,6 +156,7 @@ class Game():
                 movil.dibujate(self.pantalla)
 
             pg.display.flip()
+
         pg.quit()
 
 
